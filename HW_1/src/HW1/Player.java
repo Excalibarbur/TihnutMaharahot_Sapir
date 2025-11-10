b06d8b64ad28abcb1868f0ca839b35e659b2c29c
roiat <roiat@Roi>
2025-11-10 18:12:58 +0200

    updated Player class

--- HW_1/src/HW1/Player.java
+++ HW_1/src/HW1/Player.java
@@ -6,14 +6,49 @@ public class Player extends Character {
 	public static final int CAPACITY = 10;
 	
 	
+	// ****Builders *****
+	// default builder 
 	public Player() {
 		super();
+		this.inventory = new item[CAPACITY];
+	}
+	
+	//parameter builder
+	public Player(String name , int health , int level) {
+		super(name , health , level);
+		this.inventory = new item[CAPACITY];
+	}
+	
+	//***Methods***
+	//adding item function
+	public boolean  addItem (item item) {
+		if(item == null) return false;
+		for(int i = 0; i < inventory.length; i++) {
+			if(inventory[i] == null) {
+				inventory[i] = item;
+				return true;
+				
+			}
+		}
 		
+		return false; //no room in inventory!
+	}
+	 
+	//calculates amount of items in inventory
+	public int calculateInventory() {
+		int sum = 0;
+		for(int i = 0; i < inventory.length; i++) {
+			if(inventory[i] != null) {
+				sum += inventory[i].getValue();
+			}
+		}
+		return sum;
 	}
 	
 	
-	
-	
+	public boolean hasMagicItem() {
+		
+	}
 	
 	
 	
